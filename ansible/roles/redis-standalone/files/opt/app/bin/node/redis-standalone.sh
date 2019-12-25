74648cea84087846f21adc684b10deb5d284f746
Lizhu You <zhuliyou@yunify.com>
2019-12-25 16:14:36 +0800

    add tips

--- ansible/roles/redis-standalone/files/opt/app/bin/node/redis-standalone.sh
+++ ansible/roles/redis-standalone/files/opt/app/bin/node/redis-standalone.sh
@@ -7,6 +7,7 @@ RDB_FILE_EXIST_ERR=221
 RDB_FILE_CHECK_ERR=222
 CLUSTER_STATUS_ERR=223
 NODES_NUMS_ERR=224
+ACCEPT_ERR=225
 
 
 initNode() {
@@ -355,6 +356,11 @@ getMyRole(){
 
 
 restoreByCustomRdb(){
+  local accept; accept="$(echo $1 |jq .tipAccept)"
+  [[ "$accept" == "\"yes\"" ]] || {
+    log "user dont accept tips"
+    return $ACCEPT_ERR
+  }
   local uploadedRDBFile redisCheckRdbPath destRDBfile
   uploadedRDBFile="/data/caddy/upload/dump.rdb" redisCheckRdbPath="/opt/redis/current/redis-check-rdb" destRDBfile="/data/redis/dump.rdb"
 
